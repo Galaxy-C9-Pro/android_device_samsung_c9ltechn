bf80c5676ff6aa914f6f0d831661137ef8a9e949
travis mills <travismills82@gmail.com>
2017-08-07 14:05:32 +0800

    add wifi configs, organise device tree better

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,11 +42,24 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 
 # RIL
-TARGET_RIL_VARIANT := caf
-#BOARD_PROVIDES_LIBRIL := true
+#TARGET_RIL_VARIANT := caf
+BOARD_PROVIDES_LIBRIL := true
 
 # Telephony
 BOARD_RIL_CLASS := ../../../device/samsung/c9lte/ril
+
+# Wifi
+BOARD_HAS_QCOM_WLAN := true
+BOARD_HAS_QCOM_WLAN_SDK := true
+BOARD_WLAN_DEVICE := qcwcn
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+WIFI_DRIVER_FW_PATH_AP := "ap"
+WIFI_DRIVER_FW_PATH_STA := "sta"
+WIFI_DRIVER_FW_PATH_P2P := "p2p"
+WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # inherit from the proprietary version
 -include vendor/samsung/c9ltechn/BoardConfigVendor.mk
