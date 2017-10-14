5289e49c1cc88fa342d571f6eb441d3276678c67
travis mills <travismills82@gmail.com>
2017-10-14 21:22:22 +0800

    move libnfc-sec.conf to c9ltechn vendor

--- device.mk
+++ device.mk
@@ -22,10 +22,6 @@ $(call inherit-product-if-exists, vendor/samsung/c9ltechn/c9ltechn-vendor.mk)
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-# NFC
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/nfc/libnfc-sec.conf:system/etc/libnfc-sec.conf
-
 # Inherit from c9lte-common
 $(call inherit-product, device/samsung/c9lte-common/msm8976.mk)
 # call the proprietary setup
